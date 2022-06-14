cf0bb1e97df9289001d4a607a79cf8bc9c2b0741
cecisandoval <cecilia_sandoval@epam.com>
2022-06-14 12:37:35 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "profile"{
+    type = string
+    default = "default"
+
+}
+
+variable "region-master" {
+    type = string
+    default = "us-west-2"
+}
+
+#ami-0ee8244746ec5d6d4
+#ami-0ee8244746ec5d6d4
